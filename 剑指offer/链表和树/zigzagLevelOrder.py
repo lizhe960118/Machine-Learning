7efcfb4e1b42cb54e8e48089a886ba02f291c638
lizhe_window <2957308424@qq.com>
2019-01-02 21:33:19 +0800

    add files in jianzhi offer

--- 剑指offer/链表和树/zigzagLevelOrder.py
+++ 剑指offer/链表和树/zigzagLevelOrder.py
@@ -4,6 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from queue import Queue
 
 class Solution(object):
     def zigzagLevelOrder(self, root):
@@ -11,4 +12,25 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        
+        q = Queue()
+        if root is None:
+            return []
+        q.put(root)
+        flag = -1
+        s = []
+        while not q.empty():
+            l = []
+            new_q = Queue()
+            for node in q:
+                l.append(node.val)
+                if node.left:
+                    new_q.put(node.left)
+                if node.right:
+                    new_q.put(node.right)
+            if flag == -1:
+                s.append(l)
+            if flag == 1:
+                s.append(l[::-1])
+            q = new_q
+            flag *= -1
+        return s
