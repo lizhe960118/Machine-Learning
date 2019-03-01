c95179995dc460a4e53bc5305eab214c17d9036a
lizhe_window <2957308424@qq.com>
2019-03-01 22:38:05 +0800

    finish code in pointers

--- 剑指offer/指针运用/threeSumClosest.py
+++ 剑指offer/指针运用/threeSumClosest.py
@@ -1,17 +1,15 @@
 class Solution:
     def threeSumClosest(self, nums, target):
         # 先排序,固定一个数然后移动之后两个指针，每次记录最近目标值的数组，接下来要解决重复情况
-
-        n = len(nums)
-        if n <= 2:
-            return []
+        result = []
+        if nums is None or len(nums) <=2:
+            return result
         
         nums.sort()
 
-        s_tmp = nums[0] + nums[1] + nums[2]
+        min_diff = float("inf")
 
-        for i in range(n - 2):
-        
+        for i in range(len(nums) - 2):
             # 给第一个数去重
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
@@ -20,25 +18,17 @@ class Solution:
             l_right = n - 1
             
             while l_left < l_right:
-                s = nums[i] + nums[l_left] + nums[l_right]
+                cur_sum = nums[i] + nums[l_left] + nums[l_right]
+                cur_diff = abs(target - cur_sum)
 
-                dist = abs(t - s_tmp)
-                dist_new = abs(t - s)
-                if dist_new < dist:
-                    s_tmp = s
-                    ret = []
-                    ret.append([nums[i], nums[l_left], nums[l_right]])
+                if cur_diff < min_diff:
+                    min_diff = cur_diff
+                    result = []
+                    result.append([nums[i], nums[l_left], nums[l_right]])
 
-                if s < target:
+                if cur_sum < target:
                     l_left += 1
-                elif s > target:
-                    l_right -= 1
                 else:
-                    ret = []
-                    ret.append([nums[i], nums[l_left], nums[l_right]])
-                    break                    # l_left += 1
-                    # # 给第二个数去重
-                    # while nums[l_left] == nums[l_left - 1] and l_left < l_right:
-                    #     l_left += 1
+                    l_right -= 1
 
-        return ret
+        return cur_diff
